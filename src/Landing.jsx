1e899a1f29204291ecc664b4e5af41b07c736cb1
Vijay <unknown77709@outlook.com>
2025-07-29 14:58:45 +0530

    modified color

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -2,7 +2,7 @@ function Landing() {
 
   return (
     <div className="h-screen overflow-hidden">
-    <header className="p-4 bg-gray-100 dark:bg-gray-800 shadow-md">
+    <header className="p-4 bg-gray-800 shadow-md">
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="text-2xl font-bold text-white">Online Exam Portal</h1>
         <nav className="flex space-x-4 text-white">
